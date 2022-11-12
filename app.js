ef52745d50b0a62ca64df5eceb7abd5299d3d245
Pancax <dakotasoles@gmail.com>
2022-11-12 16:32:41 -0600

    socket

--- app.js
+++ app.js
@@ -1,19 +1,40 @@
+//env
+require('dotenv').config();
+
+//imports
 const createError = require('http-errors');
+const http = require('http');
 const express = require('express');
 const logger = require('morgan');
+const fs = require('fs');
+const path = require('path');
 
-const indexRouter = require('./routes/index');
-
+//vars
 const app = express();
+const server = http.createServer(app);
+const { Server } = require('socket.io');
+const io = new Server(server);
 
+//setup app
 app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
-app.use('/',indexRouter);
 
-//catch 404 and forward to error handler
-app.use(function(req,res,next){
-	next(createError(404));
+//routes
+app.get('/',function(req,res){
+	res.status(200).sendFile(path.join(__dirname,'static/entry.html'));
 });
 
-app.listen(3000);
+
+server.listen(3000,function(){
+	console.log('listening on *:3000');
+});
+io.on('connection',function(socket){
+	console.log('user connected');
+
+
+	socket.on('disconnect',function(){
+		console.log('A user disconnected');
+	});
+
+});
