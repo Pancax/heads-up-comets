a6aa77cb590059770ec56fc98c1a3fdd5e8eedc4
Pancax <dakotasoles@gmail.com>
2022-11-12 17:20:31 -0600

    stuff

--- app.js
+++ app.js
@@ -20,21 +20,44 @@ app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 
+
+
 //routes
 app.get('/',function(req,res){
 	res.status(200).sendFile(path.join(__dirname,'static/entry.html'));
 });
 
 
+//user routes
+app.post('/login',async function(req,res,next){
+	let user = req.body.user;
+	let pass = req.body.pass;
+
+	let users = JSON.parse(fs.readFileSync("private/users.json"));
+
+});
+
+
+
+
 server.listen(3000,function(){
 	console.log('listening on *:3000');
 });
+
+
+
+
+
+
+
 io.on('connection',function(socket){
-	console.log('user connected');
-
-
-	socket.on('disconnect',function(){
-		console.log('A user disconnected');
+	socket.on('login',function(receivedAuth){
+		//recieved an Auth login
+		console.log("login happened");
+		console.log(receivedAuth+"");
+		socket.on('disconnect',function(){
+			//remove the Auth Login
+			console.log("kill "+receivedAuth);
+		})
 	});
-
 });
